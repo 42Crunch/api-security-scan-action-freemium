9040fc90f65633d14b5b4f9b9940033da90dbd88
cr0hn <cr0hn@cr0hn.com>
2023-11-14 17:02:05 +0100

    fix - output file

--- entrypoint_github_actions_scan.py
+++ entrypoint_github_actions_scan.py
@@ -112,7 +112,7 @@ def scan_run(running_config: RunningConfiguration, binaries: str):
     logger.debug(running_config)
 
     # Create output file name for report from input file name
-    scan_output_report = f"{running_config.api_definition}.{os.urandom(10).hex}.audit-report.json"
+    scan_output_report = f"{running_config.api_definition}.{os.urandom(10).hex()}.audit-report.json"
 
     ## Is debug mode enabled?
     logger.debug("Running in debug mode, will display all commands output")
