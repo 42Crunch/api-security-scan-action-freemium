73ad49faf739f505dbf644a3a8eae8bc0a1e3b77
cr0hn <cr0hn@cr0hn.com>
2023-11-14 14:24:10 +0100

    imp - new implementation using sdk

--- entrypoint_github_actions_scan.py
+++ entrypoint_github_actions_scan.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 
 import os
-import sys
-import gzip
-import json
-import base64
-import platform
-import subprocess
-import urllib.request
+import logging
 
-from typing import Tuple
 from dataclasses import dataclass
+
+from xliic_sdk.helpers import get_binary_path, execute
+from xliic_sdk.vendors import github_running_configuration, display_header, upload_sarif
+
+logger = logging.getLogger(__name__)
 
 
 class ExecutionError(Exception):
@@ -23,14 +21,13 @@ class RunningConfiguration:
     # Configurable parameters
     #
     target_url: str
-    api_definition: str
-    log_level: str = "INFO"
+    log_level: str = "info"
     data_enrich: bool = False
     upload_to_code_scanning: bool = False
+    api_definition: str = None
     api_credential: str = None
 
     sarif_report: str = None
-    export_as_pdf: str = None
 
     # Internal parameters
     github_token: str = None
@@ -48,7 +45,6 @@ RunningConfiguration:
     data_enrich: {self.data_enrich}
     upload_to_code_scanning: {self.upload_to_code_scanning}
     sarif_report: {self.sarif_report}
-    export_as_pdf: {self.export_as_pdf}
     github_token: {self.github_token}
     github_repository: {self.github_repository}
     github_organization: {self.github_organization}
@@ -57,206 +53,62 @@ RunningConfiguration:
     github_sha: {self.github_sha}
     """
 
-    def __post_init__(self):
-        if self.log_level:
-            self.log_level = self.log_level.lower()
+    @classmethod
+    def from_github(cls) -> "RunningConfiguration":
+
+        config = github_running_configuration(
+            inputs={
+                "upload-to-code-scanning": "bool",
+                "token": "str",
+                "target-url": "str",
+                "api-definition": "str",
+                "api-credential": "str",
+                "log-level": "str",
+                "data-enrich": "bool",
+                "sarif-report": "str",
+            },
+            envs={
+                "github_repository": "str",
+                "github_repository_owner": "str",
+                "github_ref": "str",
+                "github_sha": "str",
+            }
+        )
+
+        o = cls(
+            log_level=config["log-level"],
+            data_enrich=config["data-enrich"],
+            upload_to_code_scanning=config["upload-to-code-scanning"],
+            sarif_report=config["sarif-report"],
+            target_url=config["target-url"],
+            api_definition=config["api-definition"],
+            api_credential=config["api-credential"],
+
+            github_token=config["token"],
+            github_repository=config["github_repository"],
+            github_organization=config["github_repository_owner"],
+            github_repository_owner=config["github_repository_owner"],
+            github_ref=config["github_ref"],
+            github_sha=config["github_sha"]
+        )
+
+        # Ensure log level value is valid
+        o.log_level = o.log_level.lower()
+
+        if o.log_level is None:
+            o.log_level = "info"
+        elif o.log_level not in ["fatal", "error", "warn", "info", "debug"]:
+            o.log_level = "info"
+
+        return o
 
 
-def is_security_issues_found(sarif_report: str) -> Tuple[bool, int]:
-    """
-    Check if security issues were found in the SARIF report.
-
-    If the SARIF report has no results, it means that no security issues were found. So, we return False. Otherwise, True
-    """
-    with open(sarif_report, "r") as f:
-        data = json.load(f)
-
-        try:
-            issues_found = len(data["runs"][0]["results"])
-
-            if issues_found > 0:
-                return True, issues_found
-            else:
-                return False, -1
-        except (KeyError, IndexError):
-            return False, -1
-
-
-def upload_sarif(github_token, github_repository, github_sha, ref, sarif_file_path):
-    if not github_token:
-        print("[!] Missing or empty GitHub token")
-        exit(1)
-
-    # Convert the SARIF file to base64 after gzip compression
-    try:
-        with open(sarif_file_path, 'rb') as f:
-            zipped_sarif = base64.b64encode(gzip.compress(f.read())).decode()
-    except FileNotFoundError:
-        print(f"[!] File {sarif_file_path} not found")
-        exit(1)
-
-    # Extract owner and repo from the provided repository
-    try:
-        owner, repo = github_repository.split('/')
-    except ValueError:
-        print(f"[!] Invalid repository {github_repository}")
-        exit(1)
-
-    # Current directory as a URL (approximation)
-    checkout_uri = f"file://{os.getcwd()}"
-
-    # Construct the request
-    url = f"https://api.github.com/repos/{owner}/{repo}/code-scanning/sarifs"
-    headers = {
-        "Authorization": f"Bearer {github_token}",
-        "Accept": "application/vnd.github+json",
-        "X-GitHub-Api-Version": "2022-11-28"
-    }
-    data = {
-        "commit_sha": github_sha,
-        "ref": ref,
-        "sarif": zipped_sarif,
-        "tool_name": "42Crunch REST API Static Security Testing",
-        "checkout_uri": checkout_uri
-    }
-    req = urllib.request.Request(url, headers=headers, data=json.dumps(data).encode())
-
-    # Send the request
-    try:
-        with urllib.request.urlopen(req) as response:
-            response.read()
-    except Exception as e:
-        print(f"[!] HTTP Error: {e}", flush=True)
-        exit(1)
-
-
-def display_header(title: str, text: str):
-    print()
-    print("!" * 80)
-    print(f"! {title}")
-    print("!")
-    print(f"{text}")
-    print("!" * 80)
-    print()
-
-
-def execute(command: str | list):
-    if type(command) is str:
-        cmd = command.split(" ")
-    else:
-        cmd = command
-
-    result = subprocess.run(cmd, capture_output=True)
-
-    if result.returncode != 0:
-        message = [f"Command failed with exit code {result.returncode}"]
-
-        if result.stdout:
-            message.append(f"stdout: {result.stdout.decode()}")
-
-        if result.stderr:
-            message.append(f"stderr: {result.stderr.decode()}")
-
-        raise ExecutionError("\n".join(message))
-
-    return result
-
-
-def get_binary_path() -> str:
-    def _get_audit_binary(_is_local_development: bool) -> str:
-
-        if _is_local_development:
-            return "/usr/local/bin/42c-ast-darwin-amd64"
-
-        else:
-
-            if platform.system() == "Windows":
-                return "/usr/local/bin/42c-ast-windows-amd64.exe"
-            elif platform.system() == "Darwin":
-                return "/usr/local/bin/42c-ast-darwin-amd64"
-            elif platform.system() == "Linux":
-                return "/usr/local/bin/42c-ast-linux-amd64"
-            else:
-                print()
-                print("[!] Unable to detect operating system from GitHub action environment variables")
-                print()
-                sys.exit(1)
-
-    def _check_binary_exists(ast_binary):
-        if not os.path.isfile(ast_binary):
-            print()
-            print(f"[!] Unable to find binary at {ast_binary}")
-            print()
-            sys.exit(1)
-
-    if os.getenv("LOCAL_DEVELOPMENT"):
-        binary_path = _get_audit_binary(True)
-    else:
-        binary_path = _get_audit_binary(False)
-
-    # Check that binaries exist
-    _check_binary_exists(binary_path)
-
-    return binary_path
-
-
-def get_running_configuration() -> RunningConfiguration:
-    def _to_bool(value: str) -> bool:
-        if value is None:
-            return False
-        elif value.lower() == "true":
-            return True
-        else:
-            return False
-
-    def _none_or_empty(value: str) -> str | None:
-        if value is None:
-            return None
-        elif value.strip() == "":
-            return None
-        else:
-            return value
-
-    data_enrich = _to_bool(os.getenv("INPUT_DATA-ENRICH", "false"))
-    upload_to_code_scanning = _to_bool(os.getenv("INPUT_UPLOAD-TO-CODE-SCANNING", "false"))
-
-    log_level = os.getenv("INPUT_LOG-LEVEL", "INFO")
-    if log_level is None:
-        log_level = "INFO"
-    elif log_level.upper() not in ["FAIL", "ERROR", "WARN", "INFO", "DEBUG"]:
-        log_level = "INFO"
-    else:
-        log_level = log_level.upper()
-
-    return RunningConfiguration(
-        log_level=log_level,
-        data_enrich=data_enrich,
-        api_definition=os.getenv("INPUT_API-DEFINITION", None),
-        sarif_report=_none_or_empty(os.getenv("INPUT_SARIF-REPORT", None)),
-        export_as_pdf=_none_or_empty(os.getenv("INPUT_EXPORT-AS-PDF", None)),
-        upload_to_code_scanning=upload_to_code_scanning,
-        target_url=os.getenv("INPUT_TARGET-URL", None),
-        api_credential=os.getenv("INPUT_API-CREDENTIAL", None),
-
-        github_token=os.getenv("INPUT_TOKEN", None),
-        github_repository=os.getenv("GITHUB_REPOSITORY", None),
-        github_organization=os.getenv("GITHUB_REPOSITORY_OWNER", None),
-        github_repository_owner=os.getenv("GITHUB_REPOSITORY_OWNER", None),
-        github_ref=os.getenv("GITHUB_REF", None),
-        github_sha=os.getenv("GITHUB_SHA", None)
-    )
-
-
-def scan_run(running_config: RunningConfiguration, base_dir: str, binaries: str):
+def scan_run(running_config: RunningConfiguration, binaries: str):
+    base_dir = os.getcwd()
 
     # Create output file name for report from input file name
     openapi_file = os.path.splitext(os.path.basename(running_config.api_definition))[0]
     scan_output_report = os.path.join(base_dir, f"{openapi_file}.audit-report.json")
-
-    # TODO: FIX THIS
-    openapi_file = "vampy-openapi"
-    running_config.api_definition = "vampy-openapi.json"
-    scan_output_report = "vampy-openapi.scan-result.json"
 
     #
     # Run 42Crunch cli scan
@@ -267,21 +119,17 @@ def scan_run(running_config: RunningConfiguration, base_dir: str, binaries: str)
         "run",
         "local",
         "-b", binaries,
-        "-i", base_dir,
+        "-i", running_config.api_definition,
         "-r", scan_output_report,
-        "-c",  # Copy original OpenAPI file that generated report
+        "-a", running_config.api_credential,
+        "-t", running_config.target_url,
         "--github-user", running_config.github_repository_owner,
         "--github-org", running_config.github_organization,
         "--log-level", running_config.log_level
     ]
 
-    if running_config.data_enrich:
-        audit_cmd.append("--enrich")
-
     try:
-        # Binary fix pending
-        # execute(audit_cmd)
-        pass
+        execute(audit_cmd)
     except ExecutionError as e:
         display_header("Audit command failed", str(e))
         exit(1)
@@ -321,10 +169,6 @@ def scan_run(running_config: RunningConfiguration, base_dir: str, binaries: str)
     # Upload to GitHub code scanning
     #
     if running_config.upload_to_code_scanning:
-        print("#" * 80)
-        print("Uploading to GitHub code scanning")
-        print("#" * 80)
-
         upload_sarif(
             github_token=running_config.github_token,
             github_repository=running_config.github_repository,
@@ -335,12 +179,26 @@ def scan_run(running_config: RunningConfiguration, base_dir: str, binaries: str)
 
 
 def main():
-    binary = get_binary_path()
-    current_dir = os.getcwd()
-    running_config = get_running_configuration()
+    try:
+        binary_path = get_binary_path()
+    except ExecutionError as e:
+        logger.error(display_header("Unable to get 42c-ast binary", str(e)))
+        exit(1)
+
+    try:
+        running_config = RunningConfiguration.from_github()
+    except ValueError as e:
+        logger.error(display_header("Invalid configuration", str(e)))
+        exit(1)
+
+    # Currently, only two log levels are supported: INFO and DEBUG
+    if running_config.log_level == "DEBUG":
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
 
     # Run discovery
-    scan_run(running_config, current_dir, binary)
+    scan_run(running_config, binary_path)
 
 
 # Main script execution
